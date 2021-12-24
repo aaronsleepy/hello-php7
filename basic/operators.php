fe3c4e9c70b88d5288418c2e6552d8cd95c3fb5c
aaronsleepy <aaron@kmong.com>
2021-12-24 20:24:00 +0900

    task: PHP 연산자

--- basic/operators.php
+++ basic/operators.php
@@ -1,1 +1,17 @@
 <?php
+// 대입 연산자
+$foo = 'bar';
+$foo .= 'baz';
+echo "{$foo}\n";
+
+// 산술 연산자
+$foo = 1 + "2";
+echo "{$foo}\n";
+
+// 비교 연산자
+var_dump(0 == false);
+var_dump(0 === false);
+
+// 논리 연산자
+var_dump(true && false);
+var_dump(true || false);
